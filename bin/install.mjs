9bc035364731e9633b9a7b892c3164edfe938b4c
EakkawatP <eakkawat.prommana.dev@gmail.com>
2024-02-13 20:47:46 +0700

    Project starter   - CSS Reset

---
+++ bin/install.mjs
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+import { execSync } from 'child_process';
+
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (err) {
+    console.error(`Failed to execute ${command}`, err);
+    return false;
+  }
+
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 git@github.com:eakkawat/vite-react-ts-starter.git ${repoName}`;
+
+const installDependenciesCommand = `cd ${repoName} && pnpm install`;
+
+console.info(`Cloning the repository named ${repoName}`);
+const checkOut = runCommand(gitCheckoutCommand);
+if (!checkOut) process.exit(-1);
+
+console.info(`Installing dependencies for ${repoName}`);
+const installDeps = runCommand(installDependenciesCommand);
+if (!installDeps) {
+  console.error(
+    'Please check that you have pnpm installed as we use pnpm to install packages: npm ls -g'
+  );
+  process.exit(-1);
+}
+
+console.info(
+  'Congratulations! You are ready. Follow the following command to start'
+);
+console.info(`cd ${repoName} && pnpm run dev`);
